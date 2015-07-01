ed07e6a5fc21ef8cd71f1e24008f23ab35af6e2d
Kishanthan <kshanth2101@gmail.com>
2015-07-01 18:18:21 +0530

    adding example requests files

---
+++ wso2-esb/header-based-routing/resources/example-requests/QueueSender.java
@@ -0,0 +1,36 @@
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.Queue;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+public class QueueSender {
+    public static void main(String[] args) throws JMSException {
+        ConnectionFactory factory = new ActiveMQConnectionFactory("admin", "admin", "tcp://localhost:61616");
+
+        Connection connection = factory.createConnection();
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        Queue queue = session.createQueue("JMSIn");
+
+        MessageProducer producer = session.createProducer(queue);
+
+        TextMessage msg = session.createTextMessage();
+        msg.setText(
+                "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\">\n" +
+                "       <soapenv:Body>" +
+                "           <m0:CheckPriceRequest xmlns:m0=\"http://services.samples\">" +
+                "               <m0:Code>ACT</m0:Code>" +
+                "           </m0:CheckPriceRequest>" +
+                "       </soapenv:Body>\n" +
+                "</soapenv:Envelope>");
+
+        msg.setStringProperty("ClientId", "App1");
+        producer.send(msg);
+        session.close();
+        connection.close();
+    }
+}
